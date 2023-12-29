ab54a8391271354d461095d55972966204e0129b
Benjamin <benjaminklarer@gmail.com>
2023-12-29 11:59:29 -0700

    Create hero component; Create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,41 @@
+"use client";
+
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <Hero />
+      <Hero title={["BENJAMIN", "KLARER"]} split={true}>
+        <TypeAnimation
+          sequence={[
+            1000,
+            "JavaScript",
+            1000,
+            "JavaScript\nTypeScript",
+            1000,
+            "JavaScript\nTypeScript\nReact.js",
+            1000,
+            "JavaScript\nTypeScript\nReact.js\nNext.js",
+            1000,
+            "JavaScript\nTypeScript\nReact.js\nNext.js\nRuby",
+            1000,
+            "JavaScript\nTypeScript\nReact.js\nNext.js\nRuby\nRails",
+            1000,
+            "JavaScript\nTypeScript\nReact.js\nNext.js\nRuby\nRails\nMaterial UI",
+            1000,
+            "JavaScript\nTypeScript\nReact.js\nNext.js\nRuby\nRails\nMaterial UI\nTailwind CSS",
+          ]}
+          speed={50}
+          style={{
+            fontSize: "3em",
+            whiteSpace: "pre-line",
+            display: "block",
+          }}
+          repeat={0}
+        />
+      </Hero>
     </main>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,29 @@
-const Hero = () => {
-  return (
+import React from "react";
+
+interface HeroProps {
+  children?: React.ReactNode;
+  title: string[];
+  split?: boolean;
+}
+
+const Hero = ({ children, title, split }: HeroProps) => {
+  return split ? (
     <div className="h-[90vh] w-full mt-16 text-white px-10">
-      <div className="h-full flex">
-        <div className="flex-1 border-white border "></div>
-        <div className="flex-1 border-white border "></div>
+      <div className="h-full flex justify-center w-full gap-20">
+        <div className="flex-1 grid items-center text-[7em] text-right">
+          <ul>
+            {title.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
+        </div>
+        <div className="flex-1 grid items-center">{children}</div>
+      </div>
+    </div>
+  ) : (
+    <div className="h-[90vh] w-full mt-16 text-white px-10">
+      <div className="h-full grid place-items-center w-full gap-20">
+        {children}
       </div>
     </div>
   );
