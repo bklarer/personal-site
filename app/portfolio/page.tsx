813f1b781b440df2c4acd468d83aff44e0b90ffa
Benjamin Klarer <benjaminklarer@gmail.com>
2024-01-01 16:13:24 -0700

    Make cards fit on 1 page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,7 +3,7 @@ import PortfolioGrid from "@/components/PortfolioGrid";
 
 const Portfolio = ({}) => {
   return (
-    <div className="mt-[50px] md:mt-[150px]">
+    <div className="mt-[50px] md:mt-[70px]">
       <PortfolioGrid />
     </div>
   );
